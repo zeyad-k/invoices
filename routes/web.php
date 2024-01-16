16165edcde141dedd583ad4e003e58d1d4594c02
Zeyad Abdullatif Abdullwahab Korkor <zayad30982025@f-eng.tanta.edu.eg>
2024-01-16 22:02:47 +0200

    start

---
+++ routes/web.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\InvoicesController;
+use App\Http\Controllers\SectionsController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+
+
+Route::get('/', function () {
+    return view(view: 'auth.login');
+});
+
+// Route::get('/page', 'AdminController@index');
+
+// Auth::routes();
+Auth::routes(['register' => false]);
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::resource('invoices', InvoicesController::class);
+Route::resource('sections', SectionsController::class);
+Route::get('/{page}', [AdminController::class, 'index']);
+
+// Route::resource('posts', PostController::class);
+
+// Route::resource('/invoices', 'InvoicesController');
+// Auth::routes();
+
+// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
