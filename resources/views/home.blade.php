ed5d09b71c0eefd2c589f3d3a1bafaba7db0e66d
Zeyad Abdullatif Abdullwahab Korkor <zayad30982025@f-eng.tanta.edu.eg>
2024-02-04 23:09:37 +0200

    Update home.blade.php with Arabic translations for invoice statistics

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -45,17 +45,20 @@
             <div class="card overflow-hidden sales-card bg-primary-gradient">
                 <div class="pl-3 pt-3 pr-3 pb-2 pt-0">
                     <div class="">
-                        <h6 class="mb-3 tx-12 text-white">TODAY ORDERS</h6>
+                        <h6 class="mb-3 tx-12 text-white"> اجمالي الفواتير </h6>
                     </div>
                     <div class="pb-0 mt-0">
                         <div class="d-flex">
                             <div class="">
-                                <h4 class="tx-20 font-weight-bold mb-1 text-white">$5,74.12</h4>
-                                <p class="mb-0 tx-12 text-white op-7">Compared to last week</p>
+                                <h4 class="tx-20 font-weight-bold mb-1 text-white">
+                                    {{ number_format(\App\models\invoices::sum('Total'), 2) }}$
+                                </h4>
+                                <p class="mb-0 tx-12 text-white op-7">عدد الفواتير :
+                                    {{ \App\models\invoices::count() }}</p>
                             </div>
                             <span class="float-right my-auto mr-auto">
                                 <i class="fas fa-arrow-circle-up text-white"></i>
-                                <span class="text-white op-7"> +427</span>
+                                <span class="text-white op-7"> 100%</span>
                             </span>
                         </div>
                     </div>
@@ -67,17 +70,22 @@
             <div class="card overflow-hidden sales-card bg-danger-gradient">
                 <div class="pl-3 pt-3 pr-3 pb-2 pt-0">
                     <div class="">
-                        <h6 class="mb-3 tx-12 text-white">TODAY EARNINGS</h6>
+                        <h6 class="mb-3 tx-12 text-white"> الفواتير الغير مدفوعة </h6>
                     </div>
                     <div class="pb-0 mt-0">
                         <div class="d-flex">
                             <div class="">
-                                <h4 class="tx-20 font-weight-bold mb-1 text-white">$1,230.17</h4>
-                                <p class="mb-0 tx-12 text-white op-7">Compared to last week</p>
+                                <h4 class="tx-20 font-weight-bold mb-1 text-white">
+                                    {{ number_format(\App\models\invoices::where('Value_Status', 2)->sum('Total'), 2) }} $
+                                </h4>
+                                <p class="mb-0 tx-12 text-white op-7">عدد الفواتير :
+                                    {{ \App\models\invoices::where('Value_Status', 2)->count() }}</p>
                             </div>
                             <span class="float-right my-auto mr-auto">
                                 <i class="fas fa-arrow-circle-down text-white"></i>
-                                <span class="text-white op-7"> -23.09%</span>
+                                <span class="text-white op-7">
+                                    {{ round((\App\models\invoices::where('Value_Status', 2)->count() / \App\models\invoices::count()) * 100) }}
+                                    %</span>
                             </span>
                         </div>
                     </div>
@@ -89,17 +97,22 @@
             <div class="card overflow-hidden sales-card bg-success-gradient">
                 <div class="pl-3 pt-3 pr-3 pb-2 pt-0">
                     <div class="">
-                        <h6 class="mb-3 tx-12 text-white">TOTAL EARNINGS</h6>
+                        <h6 class="mb-3 tx-12 text-white">الفواتير المدفوعة</h6>
                     </div>
                     <div class="pb-0 mt-0">
                         <div class="d-flex">
                             <div class="">
-                                <h4 class="tx-20 font-weight-bold mb-1 text-white">$7,125.70</h4>
-                                <p class="mb-0 tx-12 text-white op-7">Compared to last week</p>
+                                <h4 class="tx-20 font-weight-bold mb-1 text-white">
+                                    {{ number_format(\App\models\invoices::where('Value_Status', 1)->sum('Total'), 2) }} $
+                                </h4>
+                                <p class="mb-0 tx-12 text-white op-7">عدد الفواتير :
+                                    {{ \App\models\invoices::where('Value_Status', 1)->count() }}</p>
                             </div>
                             <span class="float-right my-auto mr-auto">
                                 <i class="fas fa-arrow-circle-up text-white"></i>
-                                <span class="text-white op-7"> 52.09%</span>
+                                <span class="text-white op-7">
+                                    {{ round((\App\models\invoices::where('Value_Status', 1)->count() / \App\models\invoices::count()) * 100) }}
+                                    %</span>
                             </span>
                         </div>
                     </div>
@@ -111,17 +124,22 @@
             <div class="card overflow-hidden sales-card bg-warning-gradient">
                 <div class="pl-3 pt-3 pr-3 pb-2 pt-0">
                     <div class="">
-                        <h6 class="mb-3 tx-12 text-white">PRODUCT SOLD</h6>
+                        <h6 class="mb-3 tx-12 text-white"> الفواتير المدفوعة جزئيا </h6>
                     </div>
                     <div class="pb-0 mt-0">
                         <div class="d-flex">
                             <div class="">
-                                <h4 class="tx-20 font-weight-bold mb-1 text-white">$4,820.50</h4>
-                                <p class="mb-0 tx-12 text-white op-7">Compared to last week</p>
+                                <h4 class="tx-20 font-weight-bold mb-1 text-white">
+                                    {{ number_format(\App\models\invoices::where('Value_Status', 3)->sum('Total'), 2) }} $
+                                </h4>
+                                <p class="mb-0 tx-12 text-white op-7">عدد الفواتير :
+                                    {{ \App\models\invoices::where('Value_Status', 3)->count() }}</p>
                             </div>
                             <span class="float-right my-auto mr-auto">
                                 <i class="fas fa-arrow-circle-down text-white"></i>
-                                <span class="text-white op-7"> -152.3</span>
+                                <span class="text-white op-7">
+                                    {{ round((\App\models\invoices::where('Value_Status', 3)->count() / \App\models\invoices::count()) * 100) }}
+                                    %</span>
                             </span>
                         </div>
                     </div>
